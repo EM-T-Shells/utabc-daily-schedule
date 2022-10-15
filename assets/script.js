df6a88a29c4edd30af91db33b1ba396abdc592ec
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-10-14 19:12:00 -0500

    html template set for js. will style at end.

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,23 @@ if (today){
   today.textContent = moment().format('MM/DD/YYYY');
 }
 
+//?insert momentjs time format into timeToDo or html?
+//allow user input into whatToDo
+//allow user to save whatToDo in saveToDo
+/*saveToDo button needs event listener -> store saved whatToDo in local storage*/
+//different block colors for past, next up, future whatToDo's
+
+
+//extra fun: reminder modal
+
+
+/*<tbody class="plannerBody">
+<tr class="plannerRow">
+<td class="timeToDo">0600</td>
+<td class="whatToDo">Otto</td>
+<td class="saveToDo">
+  <button class="btn saveItBtn">[]</button>
+</td>
+</tr>
+</tbody>
+*/
