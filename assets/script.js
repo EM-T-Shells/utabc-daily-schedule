5f4d4b655c588c9782255e991c37b051b464f347
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-10-18 23:48:07 -0500

    js finished

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,187 @@
+//current time
+
+//console.log(dayJsObject.format("h:mm A"));
 //header date
+let currentDate = document.getElementById('currentDate').innerHTML = dayjs().format("M/D/YYYY h:mm A");
 
-const dayJsObject = dayjs();
-console.log(dayJsObject.format("M/D/YYYY h:mm A"));
-let currentDate = document.getElementById('currentDate').innerHTML = dayJsObject.format("M/D/YYYY h:mm A");
+let dayJsObject = dayjs().format("hh");
+console.log(dayJsObject);
 
-var currentTime = prompt
+var whatTodoToday = [
+  {
+      id: "0",
+      hour: "08",
+      time: "08",
+      meridiem: "am",
+      todo: ""
+  },
+  {
+    id: "1",
+    hour: "09",
+    time: "09",
+    meridiem: "am",
+    todo: ""
+},
+  {
+      id: "2",
+      hour: "10",
+      time: "10",
+      meridiem: "am",
+      todo: ""
+  },
+  {
+      id: "3",
+      hour: "11",
+      time: "11",
+      meridiem: "am",
+      todo: ""
+  },
+  {
+      id: "4",
+      hour: "12",
+      time: "12",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+      id: "5",
+      hour: "01",
+      time: "13",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+      id: "6",
+      hour: "02",
+      time: "14",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+      id: "7",
+      hour: "03",
+      time: "15",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+      id: "8",
+      hour: "04",
+      time: "16",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+      id: "9",
+      hour: "05",
+      time: "17",
+      meridiem: "pm",
+      todo: ""
+  },
+  {
+    id: "10",
+    hour: "06",
+    time: "18",
+    meridiem: "pm",
+    todo: ""
+  },
+  {
+    id: "11",
+    hour: "07",
+    time: "19",
+    meridiem: "pm",
+    todo: ""
+  },
+  {
+    id: "12",
+    hour: "08",
+    time: "20",
+    meridiem: "pm",
+    todo: ""
+  },
+  {
+    id: "13",
+    hour: "09",
+    time: "21",
+    meridiem: "pm",
+    todo: ""
+  },
+]
 
-//var userWakeUpTime = 
-//var userBedTime =
-for (i=0; i <= times[i].length-1; i++) {
-  if (times[i] == userTime) {
-    let firstTimeBlock = time
-    //console.log(userTime);
-    function wakeUp() {
-      if(userTime) {
-        let planBody = document.getElementById('planner');
-        let planRow = document.createElement('tr', 'plannerContent');
-        let planTime = document.createElement('td', 'plannerContent');
-
-        planTime.textContent = userTime;
-        //moment(userTime).format("HH:mm");
-        planRow.appendChild(planTime);
-        planBody.appendChild(planRow);
-      }
-    }
-  }
+function storeTodo() {
+    localStorage.setItem("whatTodoToday", JSON.stringify(whatTodoToday));
+    //adds on data from time array to localStorage
 }
 
+function showTodo() {
+  whatTodoToday.forEach(function (_thisHour) {
+      $(`#${_thisHour.id}`).val(_thisHour.todo);
+  })
+  //any stored data will appear on planner
+}
 
+function init() {
+  var storedTodo = JSON.parse(localStorage.getItem("whatTodoToday"));
+  if (storedTodo) {
+    whatTodoToday = storedTodo;
+  }
+  storeTodo();
+  showTodo();
+}
 
+whatTodoToday.forEach(function(thisHour) {
+  //create new DOM elements for timeBlocks
+  var timeRow = $("<form>").attr({
+    "class": "row"
+  });
+  $(".container").append(timeRow);
 
+  //concat time value to am/pm
+  var timeContent = $("<div>").text(`${thisHour.hour} ${thisHour.meridiem}`).attr({
+    "class": "col-2 hour"
+  });
 
+ //
+ var hourPlan = $("<div>").attr({
+    "class": "col-8 description p-0"
+ });
 
-/*<button class="btn btn-primary" type="button">
-                <i class="fa fa-save"></i>
-              </button>*/8
+  var todoText = $("<textarea>");
+  hourPlan.append(todoText);
+  todoText.attr("id", thisHour.id);
+  if (thisHour.time < dayJsObject) {
+  todoText.attr ({
+      "class": "past col-10",
+  })
+  } else if (thisHour.time === dayJsObject) {
+  todoText.attr({
+      "class": "present col-10"
+  })
+  } else if (thisHour.time > dayJsObject) {
+  todoText.attr({
+      "class": "future col-10"
+  })
+  }
+
+// creates save button
+var saveButton = $("<i class='far fa-save fa-lg'></i>")
+var saveTodo = $("<button>")
+ .attr({
+     "class": "col-2 saveBtn"
+});
+saveTodo.append(saveButton);
+timeRow.append(timeContent, hourPlan, saveTodo);
+})
+
+//call init function here to show the stored data once page is loaded
+init();
+
+// saves data to be used in localStorage
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+  var saveDataIndex = $(this).siblings(".description").children(".future").attr("id");
+  whatTodoToday[saveDataIndex].todo = $(this).siblings(".description").children(".future").val();
+  console.log(saveDataIndex);
+  storeTodo();
+  showTodo();
+})
